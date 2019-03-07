36ecb551bc292c3a7b13e0b700a0030fd5406bcb
sergejhorvat <sergejhorvat@users.noreply.github.com>
2019-03-07 15:04:58 +0100

    Added test controllers

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,10 +8,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PropertyInjectedController {
 
-    @Autowired
+    @Autowired  //autowired called by type (hard type) (not interface) so no need to @Qualify
     public GreetingServiceImpl greetingService;
+
+    // Case 2. -  put the name of the property as the Bean name but use interface
+    // Spring will reflect that the property name matches Bean name
+    public GreetingService greetingServiceImp;
 
     public String sayHello(){
         return greetingService.sayGreetings();
+        // Case 2. - use proper property name, also thange should be done in test PropertyInjectedControllerTest
+        //return greetingServiceImp.sayGreetings();
     }
 }
